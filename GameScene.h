358bb7f0340de3cc021288db7b87afe294b1a8c7
yuta-machida-TEF <k024g0029@g.neec.ac.jp>
2026-05-15 12:58:43 +0900

    プロジェクトファイルを追加。

---
+++ GameScene.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "KamataEngine.h"
+
+// ゲームシーン
+class GameScene {
+
+	public:
+	// 初期化
+	void Initialize();
+
+	// 更新
+	void Update();
+
+	// 描画
+	void Draw();
+};
---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <Windows.h>
+#include "KamataEngine.h"
+#include "GameScene.h"
+
+using namespace KamataEngine;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	
+	// エンジンの初期化
+	KamataEngine::Initialize(L"LE3D_23_マチダ_ユウタ_CG4_02");
+
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// ゲームシーンのインスタンス生成
+	GameScene* gameScene = new GameScene();
+	// ゲームシーンの初期化
+	gameScene->Initialize();
+
+	// メインループ
+	while (true) {
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		// ゲームシーンの更新
+		gameScene->Update();
+
+		// 描画開始
+		dxCommon->PreDraw();
+
+		// ゲームシーンの描画
+		gameScene->Draw();
+
+		// 描画終了
+		dxCommon->PostDraw();
+	}
+
+	// ゲームシーンの解放
+	delete gameScene;
+	
+	// nullptrの代入
+	gameScene = nullptr;
+
+	// エンジンの終了処理
+	KamataEngine::Finalize();
+
+	return 0;
+}
